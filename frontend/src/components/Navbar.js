00108fa8048527d14f99a78b7533d9d11dab8503
chandan pal <chandanpal18222gmail.com>
2024-08-23 21:19:47 +0530

    login & signup form

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,8 +6,14 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Workout Karo Guys</h1>
+          <h1>Workout Buddy</h1>
         </Link>
+        <nav>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
       </div>
     </header>
   )
