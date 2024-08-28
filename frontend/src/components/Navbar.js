17f3c64220f8f075796a67a72b96275d666b9ce9
chandan pal <chandanpal18222gmail.com>
2024-08-28 20:47:28 +0530

    useSignup and useLogout hook

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,24 @@
 import { Link } from 'react-router-dom'
+import logo from '../images/logo.jpg'
+import { useLogout } from '../hooks/useLogout'
 
 const Navbar = () => {
+  const { logout } = useLogout()
+
+  const handleClick = () => {
+    logout()
+  }
 
   return (
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Workout Buddy</h1>
+        <img src={logo} alt="Workout Buddy Logo" className="logo" />
         </Link>
         <nav>
+        <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
